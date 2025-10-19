6cb665082d3c93b1683dd15ea58807bd6abeedda
Nazar14321 <Komaretskiy.nazar@lll.kpi.ua>
2025-10-19 19:31:46 +0300

    added post and delete and get for category(small rename update for users)

--- app/storage.py
+++ app/storage.py
@@ -1,1 +1,2 @@
 user_data={1:"Vasya",2:"Petya",3:"Amogusetya"}
+category_data={1:"Educational",2:"Entertainment",3:"Music",4:"Sports", 5:"News",}
--- app/views.py
+++ app/views.py
@@ -18,16 +18,16 @@ def get_user(user_id:int):
 
 @app.get("/users")
 def get_users():
-    return [{"id":i, "name":name} for i,name in storage.user_data.values()],200
+    return [{"id": i, "name": name} for i, name in storage.user_data.items()], 200
 
 @app.post("/user")
 def add_user():
-    user_data=request.get_json()
-    if (not user_data or "name" not in user_data ):
+    cat_data=request.get_json()
+    if (not cat_data or "name" not in cat_data ):
         return {"error": " not enough user data"},400
     user_id=max(storage.user_data.keys() or [0]) + 1
-    storage.user_data[user_id]=user_data["name"]
-    return {"id":user_id, "name":user_data["name"]}, 201
+    storage.user_data[user_id]=cat_data["name"]
+    return {"id":user_id, "name":cat_data["name"]}, 201
 
 @app.delete("/user/<int:user_id>")
 def kill_user(user_id:int):
@@ -35,4 +35,29 @@ def kill_user(user_id:int):
         return {"error": "user not found"}, 404
     name= storage.user_data.pop(user_id,None)
     return{"result":f"id: {user_id} successful deleted","name":name},200
+
+@app.get("/category")
+def get_categories():
+    return [{"id":i, "name":name} for i,name in storage.category_data.items()],200
+
+@app.post("/category")
+def add_category():
+    cat_data=request.get_json()
+    if (not cat_data or "name" not in cat_data ):
+        return {"error": " not enough user data"},400
+    record_id=max(storage.category_data.keys() or [0]) + 1
+    storage.category_data[record_id]=cat_data["name"]
+    return {"id":record_id, "name":cat_data["name"]}, 201
+
+@app.delete("/category")
+def kill_category():
+    cat_data=request.get_json()
+    if (not cat_data or "id" not in cat_data):
+        return {"error": " incorrect id to delete category_data"},400
+    category_id= int(cat_data["id"])
+    name= storage.category_data.pop(category_id,None)
+    if name is None:
+        return {"error": "category not found"}, 404
+    return {"result": f"id: {category_id} successful deleted", "name": name}, 200
+
 
